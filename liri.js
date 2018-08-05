01b0d028d167456ad8300994e9836e3a7f9b6383
Finn Logue Logue <finnlogue1@gmail.com>
2018-08-05 00:31:33 -0700

    M

--- liri.js
+++ liri.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 
 var Twitter = require("Twitter");
   
-var keys = require("keys.js")
+var keys = require("./keys.js");
 var client = new Twitter(keys.Twitter);
 
 myTweets = function() {
     console.log(client);
 }
 
-client.myTweets();
